872d753df061349e28e73da6eecc3947e9e8e6cc
Anas Ben Mustafa <anas.mustafa2301@gmail.com>
2024-02-21 19:33:49 +0300

    Initial commit

---
+++ UIKitMaraphone7/ViewController.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+final class ViewController: UIViewController {
+
+    private static var defaultImageHeight: CGFloat = 270
+    
+    // MARK: - Views
+    
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        
+        view.delegate = self
+        view.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 2)
+        
+        return view
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "Screenshot 2024-02-21 at 19.18.11")
+        return view
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        setupViews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        imageView.frame = .init(
+            x: .zero,
+            y: -scrollView.safeAreaInsets.top,
+            width: scrollView.frame.width,
+            height: Self.defaultImageHeight
+        )
+    }
+    
+    // MARK: - Setup Views
+    
+    func setupViews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+    }
+
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension ViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let y = scrollView.contentOffset.y + scrollView.safeAreaInsets.top
+        
+        if y < 0 {
+            imageView.frame = .init(x: .zero, y: -scrollView.safeAreaInsets.top + y, width: scrollView.frame.width, height: Self.defaultImageHeight + abs(y))
+        }
+        
+        scrollView.verticalScrollIndicatorInsets.top = imageView.frame.height - scrollView.safeAreaInsets.top
+    }
+}
